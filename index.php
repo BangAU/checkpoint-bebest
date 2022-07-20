ae0d104574c1b7c3ccdf811cd2f424f0a823afd1
Amar Shahukhal <amar.shahukhal@gmail.com>
2022-07-20 17:36:31 +1000

    adding aos animation

--- index.php
+++ index.php
@@ -5,8 +5,7 @@
 <div class="sitecontent">
 
     <section class= "introbanner" id="banner">
-        <div class="banner-bg"></div>
-        
+        <img src="images/banner-img.jpg" alt="" class="banner-bg">
         <div class="intro">
             <header class="logos">
                 <div class="logo-checkpoint">
@@ -16,20 +15,20 @@
             <div class="intro-text">
                 <div class="container">
                     <div class="intro-text-wrap"> 
-                        <h4>Are you ready to</h3>
-                        <h1>
+                        <h4 data-aos="fade-in" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="800">Are you ready to</h3>
+                        <h1 data-aos="fade-in" data-aos-easing="ease" data-aos-delay="1000" data-aos-duration="1500">
                             be best in the race
                             <img src="images/banner-lockup.png" alt="" class="banner-lockup">
                             <img src="images/banner-lockup-shade.png" alt="" class="banner-lockup-shade">
                         </h1>
-                        <a href="" class="btn">Start your Engines</a>
+                        <a href="" class="btn" data-aos="fade-in" data-aos-easing="ease" data-aos-delay="2000" data-aos-duration="800">Start your Engines</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="content-text">
             <div class="container">
-                <div class="content-text-wrap">
+                <div class="content-text-wrap" data-aos="fade-in" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="1000">
                     <p>Stay Ahead with Check Point Quantum Network Security</p>
                     <p>As cyber attacks become increasingly complex, even the smallest gap in your security can cause you to spin out. Ensure your business is prepared to handle unknown terrain with Check Point Quantum Network Security, the industry-leading cyber security solution.</p>
                     <p>With the best third-party independent test results for threat protection, Check Point’s innovative technology provides your business with real-time prevention, safeguarding you from even the most sophisticated attacks.</p>
@@ -44,15 +43,15 @@
     <section class="img-text" id="check-up">
         <div class="container">
             <div class="img-text-wrap">
-                <div class="img-holder">
+                <div class="img-holder" data-aos="fade-right" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="800">
                     <img src="images/img-1.png" alt="">
                 </div>
-                <div class="text-holder">
+                <div class="text-holder" data-aos="fade-in" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="800">
                     <div class="text-holder-wrap">
                         <div class="heading">
                             <h2>
                                 It's time for your check-up
-                                <img src="images/" alt="">
+                                <img src="images/heading-checkup.png" alt="">
                             </h2>
                             <h4>with Check Point!</h4>
                         </div>
@@ -71,13 +70,16 @@
     <section class="img-text pattern" id="upgrade">
         <div class="container">
             <div class="img-text-wrap">
-                <div class="img-holder">
+                <div class="img-holder" data-aos="fade-right" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="800">
                     <img src="images/img-resource.png" alt="">
                 </div>
-                <div class="text-holder">
+                <div class="text-holder" data-aos="fade-in" data-aos-easing="ease" data-aos-delay="500" data-aos-duration="800">
                     <div class="text-holder-wrap">
                         <div class="heading">
-                            <h2>5 reasons to upgrade your cyber security</h2>
+                            <h2>
+                                5 reasons to upgrade your cyber security
+                                <img src="images/heading-cyber-security.png" alt="">
+                            </h2>
                             <h4>with Check Point!</h4>
                         </div>
                         <div class="description">
@@ -95,11 +97,14 @@
     <section class="img-text" id="lets-talk">
         <div class="container">
             <div class="img-text-wrap">
-                <div class="text-holder narrow">
+                <div class="text-holder narrow"  data-aos="fade-in" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800">
                     <div class="text-holder-wrap">
                         <div class="heading">
-                            <h2>Let's Talk</h2>
-                            <h3>Shop</h3>
+                            <h2>
+                                Let's Talk
+                                <img src="images/heading-lets-talk.png" alt="">
+                            </h2>
+                            <h3 class="shine">Shop</h3>
                         </div>
                         <div class="description">
                             <p>Are you due for a tune-up? Check Point’s team of fast-mobilising experts are ready to support you with a cost-effective renewal and upgrade of your cyber security solution. We know every business has different IT needs to make for a smooth ride and we’ll work closely with you to find your ideal gear and maintenance plan.</p>
@@ -110,7 +115,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="img-holder img-right">
+                <div class="img-holder img-right" data-aos="fade-left" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800">
                     <img src="images/img-2.png" alt="">
                 </div>
             </div>
@@ -120,19 +125,20 @@
     <section class="security" id="security">
         <img class="bg-img" src="images/img-bike.png" />
         <div class="container">
+            <div class="heading">
+                <h2 data-aos="fade-in" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800">
+                    Security that goes
+                    <img src="images/heading-security.png" alt="">
+                </h2>
+                <h3 data-aos="fade-in" data-aos-easing="linear" data-aos-delay="1000" data-aos-duration="800">The Distance</h3>
+            </div>
             <div class="inner-container">
-                <div class="heading">
-                    <h2>
-                        Security that goes
-                    </h2>
-                    <h3>The Distance</h3>
-                </div>
                 <div class="two-cols">
-                    <div class="col">
+                    <div class="col" data-aos="fade-in" data-aos-easing="linear" data-aos-delay="1500" data-aos-duration="800">
                         <p>Check Point Quantum Network Security future-proofs your business, so you're primed for every leg of the race. No matter how expansive your operation or the size of your workforce, we’ll keep you covered as you speed toward your goals.</p>
                         <p>Our hyper-scalable solution protects your network, cloud, remote workforce, IoT and data centre from all cyber threats, including advanced Gen V cyber attacks. And with faster, more efficient performance from our latest non-ASIC appliances, you’ll naturally lower your costs and energy usage for a greener operation.</p>
                     </div>
-                    <div class="col">
+                    <div class="col"data-aos="fade-in" data-aos-easing="linear" data-aos-delay="2000" data-aos-duration="800">
                         <p>
 With Check Point in your side-car, you can safely handle the sharpest turns and keep your eyes on the prize.</p>
                         <div class="btn-holder">
@@ -146,19 +152,19 @@ With Check Point in your side-car, you can safely handle the sharpest turns and 
 
     <section class="ready" id="ready">
         <div class="container">
-            <img src="images/img-3.png"class="bg-img"/>
+            <img src="images/img-3.png"class="bg-img" data-aos="fade-in" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800"/>
             <div class="inner-container">
                 <div class="three-cols">
-                    <div class="col">
+                    <div class="col"data-aos="fade-right" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800">
                         <h2>Ready…</h2>
                         <p>Be prepared for anything with Check Point's uncompromising security and agility. Our ground-breaking AI technology blocks emerging zero-day threats and acquires protections from the latest attacks. We won't just detect a risk - we'll prevent it. </p>
                         <p>And with at least 80% of network traffic requiring decryption, as well as a 25% increase of network traffic year-over-year, the capacity and speed of our technology is crucial to keeping your business competitive whilst remaining secure. Our scalable solution minimises costs while increasing performance, encouraging the advancement of your operations. </p>
                     </div>
-                    <div class="col">
+                    <div class="col"data-aos="fade-right" data-aos-easing="linear" data-aos-delay="750" data-aos-duration="800">
                         <h2>Set…</h2>
                         <p>Check Point's bespoke security solutions are best-in-market, flexible and easy to manage. With a consolidated IT system, you'll have one licensing environment and options for stacking multiple devices to add extra capacity at any time. Our stock is readily available, plus you'll have access to future proof your investment  with trade-in value and competitive buy-outs with our Infinity ELA.</p>
                     </div>
-                    <div class="col">
+                    <div class="col"data-aos="fade-right" data-aos-easing="linear" data-aos-delay="1000" data-aos-duration="800">
                         <h2>Go!</h2>
                         <p>With the best IT and best scalable security solution, you'll go further, faster. Check Point's efficient technology and unmatched client support position you to confidently handle the ongoing growth and evolution of your business - even cutting your security operations by up to 80%. </p>
                         <p>By partnering with us, you'll be doing more for the environment too. We're committed to going green with sustainable products and services that reduce your footprint. That's security for your business and the planet. And we can prove it - just ask us!</p>
@@ -171,11 +177,14 @@ With Check Point in your side-car, you can safely handle the sharpest turns and 
     <section class="contact" id="contact">
         <img src="images/img-lights.jpg"class="bg-img"/>
         <div class="container">
-            <div class="form-wrap">
-                <div class="form-heading">
-                    <h2>Start your engines!</h2>
-                    <h3>Purchase or Upgrade with Check point today</h3>
-                </div>
+            <div class="form-heading">
+                <h2 data-aos="fade-in" data-aos-easing="linear" data-aos-delay="500" data-aos-duration="800">
+                    Start your engines!
+                    <img src="images/heading-contact.png" alt="">
+                </h2>
+                <h3 data-aos="fade-in" data-aos-easing="linear" data-aos-delay="800" data-aos-duration="800">Purchase or Upgrade with Check point today</h3>
+            </div>
+            <div class="form-wrap" data-aos="fade-in" data-aos-easing="linear" data-aos-delay="1000" data-aos-duration="800">
                 <form action="contact.php" method="post" id="contact-form" class="getstarted-form">
                     <fieldset>
                         <div class="form-group">
